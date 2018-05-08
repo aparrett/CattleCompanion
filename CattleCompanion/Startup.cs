274041d51a9b9622b7870914a982f90cb040120c
Anthony Parrett <anthonyparrett7@gmail.com>
2018-05-07 22:35:30 -0500

    Add project files.

---
+++ CattleCompanion/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CattleCompanion.Startup))]
+namespace CattleCompanion
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
