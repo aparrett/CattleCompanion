e183ea7f84c09b3361c6b8de7367ceced81f9707
Anthony Parrett <anthonyparrett7@gmail.com>
2018-05-09 19:54:50 -0500

    Setup Integration Tests

---
+++ CattleCompanion.IntegrationTests/GlobalSetup.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using System.Data.Entity.Migrations;
+using CattleCompanion.Models;
+using System.Linq;
+
+namespace CattleCompanion.IntegrationTests
+{
+    [SetUpFixture]
+    public class GlobalSetup
+    {
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            MigrateDbToLatestVersion();
+        }
+
+        private static void MigrateDbToLatestVersion()
+        {
+            var configuration = new CattleCompanion.Migrations.Configuration();
+            var migrator = new DbMigrator(configuration);
+            migrator.Update();
+        }
+
+        public void Seed()
+        {
+            var context = new ApplicationDbContext();
+
+            if (context.Users.Any())
+                return;
+
+            context.Users.Add(new ApplicationUser
+            {
+                UserName = "user1",
+                Email = "-",
+                PasswordHash = "-"
+            });
+
+            context.Users.Add(new ApplicationUser
+            {
+                UserName = "user2",
+                Email = "-",
+                PasswordHash = "-"
+            });
+
+            context.SaveChanges();
+        }
+    }
+}
--- CattleCompanion/Migrations/Configuration.cs
+++ CattleCompanion/Migrations/Configuration.cs
@@ -1,11 +1,8 @@
 namespace CattleCompanion.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<CattleCompanion.Models.ApplicationDbContext>
+    public sealed class Configuration : DbMigrationsConfiguration<CattleCompanion.Models.ApplicationDbContext>
     {
         public Configuration()
         {
