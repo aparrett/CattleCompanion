b01ef59261f09dea4ddde14b4fdd3532e690ec04
Anthony Parrett <anthonyparrett7@gmail.com>
2018-06-02 10:06:04 -0500

    Reset CowEvent modal upon creation

--- CattleCompanion/Scripts/app/cowDetails.js
+++ CattleCompanion/Scripts/app/cowDetails.js
@@ -7,14 +7,14 @@
         $(document).on('click', '.delete-cow', showDeleteConfirmation);
     };
 
-    var createCowEvent = function () {
+    var createCowEvent = function() {
         var eventId = $('#Events').val();
-        var date = $('#Date').val();
-        var description = $('#Description').val();
+        var date = $('#CowEventDate').val();
+        var description = $('#CowEventDescription').val();
         var event = $(`#Events option[value="${eventId}"]`).text();
 
         $.post("/api/cowEvents/", { cowId: cowId, eventId: eventId, date: date, description: description })
-            .done(function (cowEvent) {
+            .done(function(cowEvent) {
                 if ($('.empty-events').length)
                     $('.empty-events').remove();
 
@@ -23,22 +23,28 @@
 
                 var html = `<li class="list-group-item list-group-item-action flex-column align-items-start">
                                 <div class="d-flex w-100 justify-content-between">
-                                    <h5 class="mb-1">${ event }</h5>
-                                    <small>${ date }</small>
+                                    <h5 class="mb-1">${event}</h5>
+                                    <small>${date}</small>
                                 </div>
-                                <p class="mb-1">${ cowEvent.description }</p>
+                                <p class="mb-1">${cowEvent.description}</p>
                             </li>`;
 
-                $('#cow-events').append(html);
+                $('#cow-events .list-group').append(html);
                 $('#addEvent').modal('hide');
+
+                resetCowEventModal();
             }).fail(function(response) {
                 alert(response.responseJSON.message);
             });
-    }
+    };
 
     var showDeleteConfirmation = function(e) {
         e.preventDefault();
         $('#deleteConfirmation').modal('show');
+    };
+
+    var resetCowEventModal = function() {
+        $('[name="Events"], #CowEventDate, #CowEventDescription').val("");
     };
 
     return {
