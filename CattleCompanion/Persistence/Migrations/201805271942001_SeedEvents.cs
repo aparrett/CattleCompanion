110dff0ec8236681fdfcbb3efd4f49c38f220f73
Anthony Parrett <anthonyparrett7@gmail.com>
2018-06-12 23:54:46 -0500

    Add real list of events to seed migration

--- CattleCompanion/Persistence/Migrations/201805271942001_SeedEvents.cs
+++ CattleCompanion/Persistence/Migrations/201805271942001_SeedEvents.cs
@@ -7,8 +7,19 @@ namespace CattleCompanion.Migrations
         public override void Up()
         {
             Sql(@"
-                INSERT INTO [dbo].[Events] ([Name]) VALUES (N'Broken Bone')
-                INSERT INTO [dbo].[Events] ([Name]) VALUES (N'Heart Attack')
+                INSERT INTO [dbo].[Events] ([Name]) VALUES (N'Assisted Delivery')
+                INSERT INTO [dbo].[Events] ([Name]) VALUES (N'Twins')
+                INSERT INTO [dbo].[Events] ([Name]) VALUES (N'Pinkeye')
+                INSERT INTO [dbo].[Events] ([Name]) VALUES (N'Lame: Join/Structural')
+                INSERT INTO [dbo].[Events] ([Name]) VALUES (N'Lame: Foreign Object')
+                INSERT INTO [dbo].[Events] ([Name]) VALUES (N'Lame: Fescue Foot')
+                INSERT INTO [dbo].[Events] ([Name]) VALUES (N'Lost Calf: < 1 month old')
+                INSERT INTO [dbo].[Events] ([Name]) VALUES (N'Lost Calf: > 1 month old')
+                INSERT INTO [dbo].[Events] ([Name]) VALUES (N'Bloated')
+                INSERT INTO [dbo].[Events] ([Name]) VALUES (N'Prolapse: Vaginal')
+                INSERT INTO [dbo].[Events] ([Name]) VALUES (N'Prolapse: Uterine')
+                INSERT INTO [dbo].[Events] ([Name]) VALUES (N'Prolapse: Rectal')
+                INSERT INTO [dbo].[Events] ([Name]) VALUES (N'Abscess')
             ");
         }
 
